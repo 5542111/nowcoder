f6bfe8b9a51281481cbd427364366105ab13df82
Luwis <1218089941@qq.com>
2020-03-16 17:27:01 +0800

    Singleton

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,12 +1,23 @@
 import com.sun.xml.internal.ws.wsdl.writer.UsingAddressing;
 
 public class Singleton {
-    private volatile Singleton singleton;
+    private volatile static Singleton instance;
+    private Singleton(){
 
-    public synchronized void Singleton(){
-        if (singleton==null)
-        {
-
-        }
     }
+
+    public static Singleton getInstance(){
+      if (instance==null)
+      {
+          synchronized(Singleton.class){
+              if (instance==null){
+                  instance=new Singleton();
+              }
+          }
+      }
+
+      return instance;
+    }
+
+
 }
