51d4f30527b2cb7dbbaacc3b562c8ab6ddad8068
Luwis <1218089941@qq.com>
2020-07-13 22:02:02 +0800

    添加了一个方法给58题 复杂度n

--- src/lengthOfLastWord58.java
+++ src/lengthOfLastWord58.java
@@ -5,12 +5,12 @@ public class lengthOfLastWord58 {
         lengthOfLastWord58 lengthOfLastWord = new lengthOfLastWord58();
         System.out.println(lengthOfLastWord.lengthOfLastWord2(" sdajdjsalk d sajd lkas "));
     }
-
-    public int lengthOfLastWord(String s) {
+    public int lengthOfLastWord2(String s) {
         int num = 0;
         if (s != null) {
             String[] strings = s.split(" ");
-            if (strings.length != 0) {
+            if (strings.length!=0)
+            {
                 num = strings[strings.length - 1].length();
             }
 
@@ -18,11 +18,41 @@ public class lengthOfLastWord58 {
 
         return num;
     }
+    public int lengthOfLastWord3(String s) {
 
-    public int lengthOfLastWord2(String s) {
+        s=s.trim();
+        // return s.length()-s.lastIndexOf(' ')-1;
+        if (s.length()<=1) return s.length();
+        int LastIndex = s.length()-1;
+        int i=0;
+        int j=LastIndex;
+        boolean flag=true;
+        for (j=LastIndex;j>0;j--){
+            if (s.charAt(j)!=' ')
+            {
+                flag=false;
+                break;
+            }
+
+        }
+
+        for (i=j;i>0;i--){
+
+            if (flag==false&&s.charAt(i)==' '){
+                break;
+            }
+        }
+        if (i==0) return j-i+1;
+        return j-i;
+
+    }
+
+    public int lengthOfLastWord(String s) {
 
         s=s.trim();
         return s.length()-s.lastIndexOf(' ')-1;
 
+
     }
+
 }
