362a1dc6f1d1898450b16f73dc0b26e6f0c0d877
luwis <luwiskai@qq.com>
2020-09-06 23:17:55 +0800

    dfs递归全排列

--- src/fullsort26.java
+++ src/fullsort26.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class fullsort26 {
-    private final List<List<Integer>> list= new ArrayList<>();
+    public static void main(String[] args) {
+        dfs("abc".toCharArray(), new boolean[3], 0, new ArrayList<>());
+    }
+
+    private final List<List<Integer>> list = new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-        fullsort(nums,0,nums.length-1);
+        fullsort(nums, 0, nums.length - 1);
         return list;
     }
 
@@ -19,11 +24,10 @@ public class fullsort26 {
             list.add(list1);
 
         }
-        for (int i=start;i<=end;i++)
-        {
-            swap(nums,i,start);
-            fullsort(nums,start+1,end);
-            swap(nums,i,start);
+        for (int i = start; i <= end; i++) {
+            swap(nums, i, start);
+            fullsort(nums, start + 1, end);
+            swap(nums, i, start);
         }
 
 
@@ -33,6 +37,28 @@ public class fullsort26 {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    public static void dfs(char[] chars, boolean[] charsUsed, int level, List<Character> list) {
+        //截止条件
+        if (level == chars.length) {
+            System.out.println(list);
+            return;
+        }
+        //筛选节点
+        for (int i = 0; i < chars.length; i++) {
+            char temp = chars[i];
+            //筛选
+            if (!charsUsed[i]) {
+                list.add(temp);
+                charsUsed[i] = true;
+                dfs(chars, charsUsed, level + 1, list);
+                charsUsed[i] = false;
+                list.remove(list.size() - 1);
+                charsUsed[i] = false;
+            }
+
+        }
     }
 
 }
