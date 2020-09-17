fc643627f9b11d308d336d58493bdc1df7f1bb63
luwis <luwiskai@qq.com>
2020-09-17 13:15:03 +0800

    最长子序列

--- src/lengthOfLIS300.java
+++ src/lengthOfLIS300.java
@@ -1,27 +1,26 @@
 public class lengthOfLIS300 {
+    public static void main(String[] args) {
+        System.out.println(new lengthOfLIS300().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+    }
+
     public int lengthOfLIS(int[] nums) {
         if (nums.length <= 1) return nums.length;
 
         int[] dp = new int[nums.length];
         dp[0] = 1;
 
-
+        int max = 1;
         for (int i = 1; i < nums.length; i++) {
-
+            dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (nums[j]<nums[i])
-                {
-                 dp[i]=Math.max(dp[j],dp[j]+1);
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-
-
             }
-
-            System.out.println(dp[i]);
-
+            max = Math.max(dp[i], max);
         }
 
-        return dp[nums.length-1];
+        return max;
     }
 
 }
