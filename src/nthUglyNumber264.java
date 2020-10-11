33883708f092431b5255d4081d92a84156cf9e13
luwis <luwiskai@qq.com>
2020-10-12 01:08:20 +0800

    dp求第k个丑数

--- src/nthUglyNumber264.java
+++ src/nthUglyNumber264.java
@@ -3,23 +3,23 @@ public class nthUglyNumber264 {
         int[] dp = new int[n];
         dp[0] = 1;
         int factor2 = 0, factor3 = 0, factor5 = 0;
-        for (int i = 1 ; i < n; i++) {
-            dp[i] = Math.min(dp[factor2]*2,Math.min(dp[factor3]*3,dp[factor5]*5));
-            if (dp[i]==dp[factor2]) factor2++;
-            if (dp[i]==dp[factor3]) factor3++;
-            if (dp[i]==dp[factor5]) factor5++;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.min(dp[factor2] * 2, Math.min(dp[factor3] * 3, dp[factor5] * 5));
+            if (dp[i] == dp[factor2] * 2) factor2++;
+            if (dp[i] == dp[factor3] * 3) factor3++;
+            if (dp[i] == dp[factor5] * 5) factor5++;
 
         }
         return dp[n - 1];
     }
+
     public boolean isUgly(int num) {
-        int[] nums={2,3,5};
-        for(int i:nums){
-            while(num>0&&num%i==0)
-            {
-                num/=i;
+        int[] nums = {2, 3, 5};
+        for (int i : nums) {
+            while (num > 0 && num % i == 0) {
+                num /= i;
             }
         }
-        return num==1;
+        return num == 1;
     }
 }
