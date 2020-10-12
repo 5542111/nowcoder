cf10b36c9202e2ec72a9a2f9f314a0977344adf0
luwis <luwiskai@qq.com>
2020-10-12 17:21:27 +0800

    dfs分裂二叉树的最大乘积

---
+++ src/maxProduct1339.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+public class maxProduct1339 {
+    private long total = 0;
+    private long max = 0;
+
+    public int maxProduct(TreeNode root) {
+        sum(root);
+        helper(root,total);
+        return (int)(max%1000000007L);
+    }
+
+    public long helper(TreeNode root, long total) {
+        if (root == null) {
+            return 0 ;
+        }
+        long res = 0;
+        if (root.left == null && root.right ==  null) {
+            res = root.val;
+        } else if (root.left == null) {
+            res = root.val + helper(root.right, total);
+        } else if (root.right ==  null) {
+            res = root.val + helper(root.left, total);
+        } else {
+            res = root.val + helper(root.left, total) + helper(root.right, total);
+        }
+        if (res * (total - res) > max) {
+            max = res * (total - res);
+        }
+        return res;
+    }
+
+    private void sum(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode p = root;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(p);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            total += temp.val;
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+    }
+}
