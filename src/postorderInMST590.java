efff7fcf13e388dd978b7eede14743b5257db97d
luwis <luwiskai@qq.com>
2021-02-20 18:43:05 +0800

    迭代后续遍历 N叉树

--- src/postorderInMST590.java
+++ src/postorderInMST590.java
@@ -1,41 +1,41 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
 public class postorderInMST590 {
     public List<Integer> postorder(Node root) {
-        List<Integer> list=new ArrayList<>();
-        if (root==null) return list;
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
 
-        Stack<Node> stack1=new Stack<>();
-        Stack<Node> stack2=new Stack<>();
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
         stack1.push(root);
-        while (!stack1.isEmpty()){
-            Node temp=stack1.pop();
+        while (!stack1.isEmpty()) {
+            Node temp = stack1.pop();
             stack2.push(temp);
-            while (!temp.children.isEmpty())
-            {
+            while (!temp.children.isEmpty()) {
                 stack1.push(temp.children.get(0));
                 temp.children.remove(0);
             }
-            while (!stack2.isEmpty())
-            {
+            while (!stack2.isEmpty()) {
                 list.add(stack2.pop().val);
             }
         }
-        while (!stack2.isEmpty())
-        {
+        while (!stack2.isEmpty()) {
             list.add(stack2.pop().val);
         }
-
-            return list;
+        Collections.reverse(list);
+        return list;
     }
 }
+
 class Node {
     public int val;
     public List<Node> children;
 
-    public Node() {}
+    public Node() {
+    }
 
     public Node(int _val) {
         val = _val;
