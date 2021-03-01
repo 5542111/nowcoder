1dac5edbd13be70c1b6285b682f45ead15400392
luwis <luwiskai@qq.com>
2021-03-01 21:57:31 +0800

    模糊坐标

---
+++ src/ambiguousCoordinates816.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ambiguousCoordinates816 {
+
+    public static void main(String[] args) {
+        System.out.println(new ambiguousCoordinates816().ambiguousCoordinates("(1234)"));
+    }
+
+
+    public List<String> ambiguousCoordinates(String S) {
+        List<String> ans = new ArrayList();
+        for (int i = 2; i < S.length()-1; ++i)
+            for (String left: make(S, 1, i))
+                for (String right: make(S, i, S.length()-1))
+                    ans.add("(" + left + ", " + right + ")");
+        return ans;
+    }
+
+    public List<String> make(String S, int i, int j) {
+        List<String> ans = new ArrayList();
+        for (int d = 1; d <= j-i; ++d) {
+            String left = S.substring(i, i+d);
+            String right = S.substring(i+d, j);
+            if ((!left.startsWith("0") || left.equals("0"))
+                    && !right.endsWith("0"))
+                ans.add(left + (d < j-i ? "." : "") + right);
+        }
+        return ans;
+    }
+
+
+}
