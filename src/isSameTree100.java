216ef226dce5ff272523b065c01ff337126df486
luwis <luwiskai@qq.com>
2021-03-07 22:39:19 +0800

    递归判断 是否树对称

--- src/isSameTree100.java
+++ src/isSameTree100.java
@@ -10,4 +10,15 @@ public class isSameTree100 {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
+    public boolean isSymmetricalTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.right) && isSameTree(p.right, q.left);
+        }
+    }
 }
