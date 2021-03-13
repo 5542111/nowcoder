0474a3ddd36c09a393b467d892f0b019c7520be4
luwis <luwiskai@qq.com>
2021-03-13 17:19:46 +0800

    链表排序

---
+++ src/ListNodeSort.java
@@ -0,0 +1,77 @@
+public class ListNodeSort {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = split(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    /**
+     * 归并排序  划分 子链表
+     * @param head
+     * @return
+     */
+    ListNode split(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        prev.next = null;
+        return slow;
+    }
+
+    ListNode head;
+    ListNode tail;
+
+    /**
+     * 合并子链表
+     * @param left
+     * @param right
+     * @return
+     */
+    ListNode merge(ListNode left, ListNode right) {
+        head = null;
+        tail = null;
+        ListNode q1 = left;
+        ListNode q2 = right;
+        while (q1 != null || q2 != null) {
+            if (q1 == null) {
+                append(q2);
+                q2 = q2.next;
+            } else if (q2 == null) {
+                append(q1);
+                q1 = q1.next;
+            } else if (q1.val < q2.val) {
+                append(q1);
+                q1 = q1.next;
+            } else {
+                append(q2);
+                q2 = q2.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     *
+     * @param node
+     */
+    void append(ListNode node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+}
+
