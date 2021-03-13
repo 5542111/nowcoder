ac517a97478c6e8c687588716813dc54f3b253f5
luwis <luwiskai@qq.com>
2021-03-13 18:39:47 +0800

    k个一组 翻转String

--- src/ReverseStrInK.java
+++ src/ReverseStrInK.java
@@ -1,6 +1,6 @@
 public class ReverseStrInK {
     public static void main(String[] args) {
-        System.out.println(reverseStr("abcde", 2));
+        System.out.println(reverseStr1("abcde", 2));
     }
 
     public static String reverseStr(String s, int k) {
@@ -8,19 +8,17 @@ public class ReverseStrInK {
         int flag = 1;
         for (int i = 0; i < s.length(); i += (2 * k)) {
             if (flag == 1) {
-               if (i+k-1<s.length())
-                {
+                if (i + k - 1 < s.length()) {
                     for (int j = i + k - 1; j >= i; j--) {
                         stringBuffer.append(s.charAt(j));
                     }
 
-                }
-                else {
-                    for (int j=s.length()-1;j >= i; j--) {
+                } else {
+                    for (int j = s.length() - 1; j >= i; j--) {
                         stringBuffer.append(s.charAt(j));
                     }
 
-               }
+                }
 
                 flag = 0;
                 //System.out.println(stringBuffer.toString());
@@ -28,9 +26,8 @@ public class ReverseStrInK {
 
 
             if (flag == 0) {
-                if (i+k<s.length())
-                {
-                    for (int j = i + k; j < i+2*k&&j<s.length(); j++) {
+                if (i + k < s.length()) {
+                    for (int j = i + k; j < i + 2 * k && j < s.length(); j++) {
                         stringBuffer.append(s.charAt(j));
                     }
 
@@ -47,6 +44,26 @@ public class ReverseStrInK {
 
     }
 
+    public static String reverseStr1(String s, int k) {
 
+        if (s == null || s.length() == 0) return "";
+        if (k > s.length()) return s;
+
+        int len = s.length();
+        char[] chars=s.toCharArray();
+        for (int i = 0; i < len/k ; i++) {
+            int start=i*k;
+            int end=(i+1)*k-1;
+            while(start<end){
+                char temp=chars[start];
+                chars[start]=chars[end];
+                chars[end]=temp;
+                start++;
+                end--;
+            }
+        }
+
+        return new String(chars);
+    }
 
 }
