4628cf46c9563d005de2afb48316e453c7815969
luwis <luwiskai@qq.com>
2021-04-05 02:21:16 +0800

    添加git忽略文件

--- src/longestCommonSubsequence1143.java
+++ src/longestCommonSubsequence1143.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class longestCommonSubsequence1143 {
     public static void main(String[] args) {
         new longestCommonSubsequence1143().longestCommonSubsequence("1ab2345cd", "12345ef");
@@ -13,6 +17,7 @@ public class longestCommonSubsequence1143 {
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp = new int[text1.length() + 1][text2.length() + 1];
 
+        int[][] grid=new int[text1.length()+1][text2.length()+1];
 
         for (int i = 1; i < text1.length() + 1; i++) {
             for (int j = 1; j < text2.length() + 1; j++) {
@@ -22,6 +27,9 @@ public class longestCommonSubsequence1143 {
                 }
             }
         }
+        Set<String> hs=new TreeSet<>();
+        traceBack(dp,text1.toCharArray(),text2.toCharArray(),text1.length(),text2.length(),"",hs);
+        System.out.println(hs);
         for (int i = 0; i < text1.length() + 1; i++) {
             for (int j = 0; j < text2.length() + 1; j++) {
                 System.out.print(dp[i][j] + " ");
@@ -31,8 +39,32 @@ public class longestCommonSubsequence1143 {
         return dp[text1.length()][text2.length()];
     }
 
+    public  void traceBack(int [][] dp, char [] arr1, char [] arr2, int i, int j, String lcs_str,Set<String> hs)  {
+        while (i > 0 && j > 0) {
+            if (arr1[i - 1] == arr2[j - 1]) {
+                lcs_str += arr1[i - 1];
+                i--;
+                j--;
+            } else {
+                if (dp[i][j - 1] > dp[i - 1][j])
+                    j--;
+                else if (dp[i][j - 1] < dp[i - 1][j])
+                    i--;
+                else {  // 相等的情况
+                    traceBack(dp, arr1, arr2, i - 1, j, lcs_str,hs);
+                    traceBack(dp, arr1, arr2, i, j - 1, lcs_str,hs);
+                    return;
+                }
+            }
+        }
+        hs.add(new StringBuilder(lcs_str).reverse().toString());
+        //输出最长公共子序列
+    }
+
+
     /**
      * 最长连续子串
+     *
      * @param str1
      * @param str2
      * @return
