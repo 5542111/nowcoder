b8f87775a0afe43b93daa18118f8846f71d3dc16
luwis <1218089941@qq.com>
2021-07-18 22:51:59 +0800

    合并有序数组

---
+++ src/a0088merge.java
@@ -0,0 +1,23 @@
+public class a0088merge {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int len = m + n - 1;
+
+        m = m - 1;
+        n = n - 1;
+        while (m >= 0 || n >= 0) {
+            if (m < 0) {
+                nums1[len--] = nums2[n--];
+            } else if (n < 0) {
+                nums1[len--] = nums1[m--];
+            } else {
+                if (nums1[m] > nums2[n]) {
+                    nums1[len--]=nums1[m--];
+                } else {
+                    nums1[len--]=nums2[n--];
+                }
+            }
+        }
+
+    }
+}
