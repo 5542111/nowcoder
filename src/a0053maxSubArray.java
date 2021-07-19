29928a1a01bd057086a8e8f79a5ffbb6b11fd2c3
luwis <1218089941@qq.com>
2021-07-19 21:24:20 +0800

    最大连续序列和

---
+++ src/a0053maxSubArray.java
@@ -0,0 +1,17 @@
+public class a0053maxSubArray {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int ans = nums[0];
+
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+
+}
