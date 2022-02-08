0e13391c762c587e7d63e517725c5b8eecccbfa1
luwis <luwiskai@qq.com>
2022-02-08 20:23:57 +0800

    相同的树

---
+++ src/a0100isSameTree.java
@@ -0,0 +1,11 @@
+public class a0100isSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
