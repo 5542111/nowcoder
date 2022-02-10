72624e99b2c3932fb2ba8415367be00ab0635ddc
luwis <luwiskai@qq.com>
2022-02-10 22:04:22 +0800

    快慢指针找到环的入口

---
+++ src/a0142detectCycle.java
@@ -0,0 +1,20 @@
+public class a0142detectCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) break;
+        }
+
+        if (fast == null || fast.next == null) return null;
+
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+}
