02269f7f6c98247f70de6af8b5d0dd8a5bcc00ec
luwis <luwiskai@qq.com>
2022-02-22 21:38:13 +0800

    判断平衡二叉树

---
+++ src/a0110isBalanced.java
@@ -0,0 +1,20 @@
+public class a0110isBalanced {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int left = depth(root.left);
+        int right = depth(root.right);
+        if (Math.abs(left - right) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        int left = depth(root.left);
+        int right = depth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+}
