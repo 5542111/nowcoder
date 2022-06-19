4123682661a581875e8d656f28a107b10792850b
luwis <luwiskai@qq.com>
2022-06-19 21:16:14 +0800

    前缀和解决 和为k的 子数组个数

---
+++ src/a0930numSubarraysWithSum.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.sun.tools.doclint.Entity.sum;
+
+public class a0930numSubarraysWithSum {
+    public static void main(String[] args) {
+        System.out.println(new a0930numSubarraysWithSum().numSubarraysWithSum(new int[]{1, 0, 1, 0, 1}, 2));
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        int res = 0;
+        int preSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            preSumMap.put(preSum, preSumMap.getOrDefault(preSum, 0) + 1);
+            preSum += nums[i];
+            res += preSumMap.getOrDefault(preSum-goal,0);
+        }
+        return res;
+    }
+
+}
