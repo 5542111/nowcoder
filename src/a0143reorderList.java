5d2259245417e194cc349c13df3bddb7e3f21e4b
luwis <luwiskai@qq.com>
2022-06-27 23:32:31 +0800

    快慢指针+反转列表解决 链表原地址重排序

--- src/a0143reorderList.java
+++ src/a0143reorderList.java
@@ -23,4 +23,47 @@ public class a0143reorderList {
         list.get(i).next = null;
 
     }
+
+    public void reorderList2(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        //寻找中间节点
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode left = head;
+        ListNode right = slow.next;
+        slow.next = null;
+        right = reverseListNode(right);
+        merge2List(left, right);
+    }
+
+    private void merge2List(ListNode left, ListNode right) {
+        ListNode l1;
+        ListNode l2;
+        while (left != null && right != null) {
+            l1 = left.next;
+            l2 = right.next;
+
+            left.next = right;
+            left = l1;
+
+            right.next = left;
+            right = l2;
+        }
+    }
+
+    private ListNode reverseListNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = reverseListNode(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
 }
