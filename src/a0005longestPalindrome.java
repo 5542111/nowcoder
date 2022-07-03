f29ea86b9bc1b3fd3aa4da1b2efc51058262d7f3
luwis <luwiskai@qq.com>
2022-07-03 22:40:25 +0800

    最长回文子串 中心扩散算法

--- src/a0005longestPalindrome.java
+++ src/a0005longestPalindrome.java
@@ -41,4 +41,36 @@ public class a0005longestPalindrome {
         return s.substring(end - max + 1, end + 1);
 
     }
+
+    /**
+     * 中心扩散算法
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindrome2(String s) {
+        if (s == null || s.length() == 0) return s;
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
 }
