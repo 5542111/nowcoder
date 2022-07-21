d0f09137e1fa58e5701f00bdd6b90f43143ef77b
luwis <luwiskai@qq.com>
2022-07-21 21:52:34 +0800

    二叉树剪枝

---
+++ src/a0814pruneTree.java
@@ -0,0 +1,12 @@
+public class a0814pruneTree {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.val == 0 && root.left == null && root.right == null) return null;
+        return root;
+    }
+
+
+}
