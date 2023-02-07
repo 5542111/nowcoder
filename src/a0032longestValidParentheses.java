d129672bd6565f94935962d0b95780275630a98c
luwis <luwiskai@qq.com>
2023-02-07 13:13:36 +0800

    栈 最长有效括号

---
+++ src/a0032longestValidParentheses.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class a0032longestValidParentheses {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int len = 0;
+
+        stack.push(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    len = Math.max(len, i - stack.peek());
+                }
+            }
+        }
+        return len;
+    }
+}
